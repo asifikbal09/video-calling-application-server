3cafde6a4f1671f32e58e72d7ba85cbe9b8ece0f
asifikbal09 <asif.ikbal662@gmail.com>
2024-08-10 01:10:01 +0600

    [DONE] router setup complete.

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,15 @@ import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import sendResponse from './app/utils/sendResponse';
 import httpStatus from 'http-status';
+import router from './app/router';
 
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/v1",router)
 
 app.get('/', (req: Request, res: Response) => {
   sendResponse(res,{
