0f79ab85427e6562b53c1e6806a2851b817489c6
asifikbal09 <asif.ikbal662@gmail.com>
2024-08-10 00:22:18 +0600

    [DONE] Mongoose setup and connected to database successfully.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello world');
 });
+
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,33 @@
+import mongoose from 'mongoose';
+import config from './app/config';
 import app from './app';
 
-const port = 3000;
+// let server: Server;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () =>
+      console.log(`Example app listening on port ${config.port}!`),
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
+
+// process.on('unhandledRejection', () => {
+//   console.log(`Unhandled Rejection denoted. server is shuting down...`);
+//   if (server) {
+//     server.close(() => {
+//       process.exit(1);
+//     });
+//   }
+//   process.exit(1);
+// });
+
+// process.on('uncaughtException', () => {
+//   process.exit(1);
+// });
